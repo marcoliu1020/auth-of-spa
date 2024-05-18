8eef5a02a917229872f864005b9a9e24e095084e
Marco <a0931900802@gmail.com>
2024-05-18 20:38:12 +0800

    run fomat

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,6 @@
-import Router from './Router.js';
-import Auth from './Auth.js';
+import Router from "./Router.js";
+import Auth from "./Auth.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-    Router.init();
- } );
-
+  Router.init();
+});
